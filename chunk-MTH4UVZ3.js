e58e9b5a454dd0f94521ca9198134a0a755e310f
Mereka <team@mereka.io>
2026-05-17 11:52:00 +0000

    deploy: universal back behavior + demo-account auto-login confirmed

---
+++ chunk-MTH4UVZ3.js
@@ -0,0 +1,1 @@
+import{b as e}from"./chunk-KX3L7F2C.js";import{bb as r,v as t}from"./chunk-FP3JQOZO.js";var m=()=>{let o=t(e),n=t(r);return o.isLoggedIn()?!0:n.createUrlTree(["/login"])};export{m as a};
--- chunk-P3JFC3A3.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./chunk-LJGXMFXZ.js";import{ab as r,v as t}from"./chunk-G3DPQ2O5.js";var m=()=>{let o=t(e),n=t(r);return o.isLoggedIn()?!0:n.createUrlTree(["/login"])};export{m as a};
