a643e53f005f1a1ee487dc570e016c82f08554e5
Mereka <team@mereka.io>
2026-05-26 04:12:15 +0000

    deploy: Forms page-based builder + Program Structure simplification

---
+++ chunk-DKN6XY7W.js
@@ -0,0 +1,1 @@
+import{b as e}from"./chunk-HQYHC7SS.js";import{cb as r,v as t}from"./chunk-EJ3MSPCE.js";var m=()=>{let o=t(e),n=t(r);return o.isLoggedIn()?!0:n.createUrlTree(["/login"])};export{m as a};
